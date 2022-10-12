ccaf087544f61f312a99750bbbf078e8150bc78a
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2022-10-12 00:12:14 -0400

    kinda works

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import google.cloud.texttospeech as tts
 import os
 import mechanics
 import json
+import time
+import asyncio
+import typing # For typehinting
+import functools
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
@@ -16,6 +20,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+going = True
 
 
 @bot.event
@@ -29,38 +35,91 @@ async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
 
+
 @bot.command()
 async def sab(ctx, *argv):
     f = open("bosses/sab.json")
     data = json.load(f)
-    boss_mechanics = data["mechanics"]
-    time_limit = data["timeLimit"]
-    for mechanic_data in boss_mechanics:
-        print(mechanic_data)
-        mechanic = mechanics.from_json_data(mechanic_data, time_limit)
-        mechanics_map = mechanic.get_time_to_message_dict(argv)
-        for result in mechanics_map:
-            print(result, mechanics_map[result])
+    total_list = parse_mechanics(data, *argv)
     f.close()
-    await ctx.send("test")
-
-
-def unique_languages_from_voices(voices):
-    language_set = set()
-    for voice in voices:
-        for language_code in voice.language_codes:
-            language_set.add(language_code)
-    return language_set
+    await start_mechanics(ctx, total_list)
+    await ctx.send("test sab")
 
 
 @bot.command()
-async def list_languages(ctx):
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "key.json"
-    client = tts.TextToSpeechClient()
-    response = client.list_voices()
-    languages = unique_languages_from_voices(response.voices)
+async def dhuum(ctx, *argv):
+    f = open("bosses/dhuum.json")
+    data = json.load(f)
+    total_list = parse_mechanics(data, *argv)
+    f.close()
+    await start_mechanics(ctx, total_list)
+    await ctx.send("test dhuum")
 
-    await ctx.send(f" Languages: {len(languages)} ".center(60, "-"))
+
+@bot.command()
+async def stop(ctx):
+    global going
+    going = False
+
+
+async def start_mechanics(ctx, total_list):
+    global going
+    going = True
+    loop = asyncio.get_event_loop()
+    await ctx.send("3, 2, 1, Go!")
+    start_time = time.time()
+    while len(total_list) > 0 and going:
+        passed_time = time.time() - start_time
+        next_mechanic_times = []
+        for mechanics_list in total_list:
+            mechanic = mechanics_list[0]
+            print(mechanic)
+            if passed_time >= mechanic[0]:
+                await ctx.send(mechanic[1])
+                del mechanics_list[0]
+            if len(mechanics_list) == 0:
+                total_list.remove(mechanics_list)
+            mechanic = mechanics_list[0]
+            time_to_mechanic = (start_time + mechanic[0]) - time.time()
+            next_mechanic_times.append(time_to_mechanic)
+        min_time = min(next_mechanic_times)
+        print("Sleeping ", min_time)
+        time.sleep(min_time)
+
+
+def parse_mechanics(data, *argv):
+    boss_mechanics = data["mechanics"]
+    time_limit = data["timeLimit"]
+    total_list = []
+    for mechanic_data in boss_mechanics:
+        mechanic = mechanics.from_json_data(mechanic_data, time_limit)
+        mechanics_list = mechanic.get_time_to_message_list(argv)
+        total_list.append(mechanics_list)
+        for result in mechanics_list:
+            text_to_wav(result[1])
+    print(total_list)
+    return total_list
+
+
+def text_to_wav(text):
+    filename = f"audio/{text}.wav"
+    if os.path.isfile(filename):
+        return
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "key.json"
+    language_code = "en-US"
+    voice_name = "en-US-Standard-G"
+    text_input = tts.SynthesisInput(text=text)
+    voice_params = tts.VoiceSelectionParams(
+        language_code=language_code, name=voice_name
+    )
+    audio_config = tts.AudioConfig(audio_encoding=tts.AudioEncoding.LINEAR16)
+    client = tts.TextToSpeechClient()
+    response = client.synthesize_speech(
+        input=text_input, voice=voice_params, audio_config=audio_config
+    )
+    with open(filename, "wb") as out:
+        out.write(response.audio_content)
+        print(f'Generated speech saved to "{filename}"')
 
 
 bot.run('MTAyOTEzMDYwMzAwMzY1MDA2OQ.G2LcN1.b_6Yfiw3T4ryRaqAEj5lWrshXBzhOD2BCS45o0')
--- mechanics.py
+++ mechanics.py
@@ -1,5 +1,4 @@
 
-
 def from_json_data(json_data, time_limit):
     mechanic = Mechanic(json_data["interval"], json_data["playersInvolved"], json_data["mechanicNames"],
                         json_data["warnTime"], json_data["startTime"], time_limit)
@@ -7,8 +6,7 @@ def from_json_data(json_data, time_limit):
 
 
 class Mechanic:
-
-    def __init__(self, interval, players_involved, mechanic_names, warn_time=5000, start_time=0, end_time=9999):
+    def __init__(self, interval, players_involved, mechanic_names, warn_time=5, start_time=0, end_time=9999):
         self.interval = interval
         self.players_involved = players_involved
         self.mechanic_names = mechanic_names
@@ -16,31 +14,24 @@ class Mechanic:
         self.start_time = start_time
         self.end_time = end_time
 
-    def get_time_to_message_dict(self, player_names):
-        time_to_message = {}
+    def get_time_to_message_list(self, player_names):
+        time_to_message_list = []
         i = 0
         time = self.start_time
         while time < self.end_time:
-            time_to_message[time - self.warn_time] = self.get_message(i, player_names)
+            mechanic = (time - self.warn_time, self.get_message(i, player_names))
+            time_to_message_list.append(mechanic)
             i += 1
             time += self.interval
-        return time_to_message
+        return time_to_message_list
 
     def get_message(self, index, player_names):
         mechanic_name = self.mechanic_names[index % len(self.mechanic_names)]
         if self.players_involved:
             player_name = player_names[index % len(player_names)]
-            result = "{player} on {mechanic}."
+            player_name = player_name.lower()
+            player_name = player_name.strip()
+            result = "{player} on {mechanic}"
             return result.format(player=player_name, mechanic=mechanic_name)
         else:
             return mechanic_name
-
-
-    def printMechanics(self):
-        print(self.interval)
-        print(self.players_involved)
-        print(self.mechanic_names)
-        print(self.warn_time)
-        print(self.start_time)
-        print(self.end_time)
-        return "I printed mechanics!"
